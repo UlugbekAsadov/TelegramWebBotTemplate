73286f2431f8f303e9ae5a9cf6bb28d08a445f27
Ulugbek <ulugb3k270@gmail.com>
2024-07-17 07:35:13 +0500

    feat: loader added to main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import NextTopLoader from 'nextjs-toploader';
 import { ReactNode } from 'react';
 
 import { LocaleContextProvider } from '@/context/locale.context';
@@ -22,10 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <link rel="stylesheet" href="https://video-react.github.io/assets/video-react.css" />
-      </head>
+      </Head>
       <body className={cn('bg-background container min-h-dvh', inter.className)}>
+        <NextTopLoader />
         <LocaleContextProvider>
           <ReactQueryProvider>{children}</ReactQueryProvider>
         </LocaleContextProvider>
